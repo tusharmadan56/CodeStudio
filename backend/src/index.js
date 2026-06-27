0e8e6f7bc01c2fe488c96d6894cd8ed2703c2016
tusharmadan56 <tusharmadan56@gmail.com>
2026-06-27 18:56:30 +0530

    SocketIo integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 import { PORT } from './config/serverConfig.js';
 import projectRoutes from './routes/projectRoutes.js';
 
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: '*' },
+});
 
 app.use(express.json());
 app.use(express.urlencoded());
@@ -16,6 +23,15 @@ app.get('/ping', (req, res) => {
 
 app.use('/api/v1/projects', projectRoutes);
 
-app.listen(PORT, () => {
+const editorNamespace = io.of('/editor');
+editorNamespace.on('connection', (socket) => {
+    console.log('Editor socket connected', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('Editor socket disconnected', socket.id);
+    });
+});
+
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
